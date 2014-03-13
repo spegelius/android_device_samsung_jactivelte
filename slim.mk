d725663ff1e580e57a7a1eeda34af92c8306c743
Spegelius <spegelius@gmail.com>
2014-03-13 17:41:40 +0200

    Some make options Slimmified
    Change-Id: I9a329d9ebcc1c712c073a82893f8b77149a0f293

--- slim.mk
+++ slim.mk
@@ -1,16 +1,25 @@
 $(call inherit-product, device/samsung/jactiveltexx/full_jactiveltexx.mk)
 
-# Inherit some common CM stuff.
+# Release name
+PRODUCT_RELEASE_NAME := jactiveltexx
+
+# Inherit some common SLIM stuff.
 $(call inherit-product, vendor/slim/config/gsm.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common SLIM stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit torch settings
+$(call inherit-product, vendor/slim/config/common_ledflash.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jactiveltexx TARGET_DEVICE=jactiveltexx BUILD_FINGERPRINT="samsung/jactiveltexx/jactivelte:4.2.2/JDQ39/I9295XXUAMF7:user/release-keys"
 
 PRODUCT_NAME := slim_jactiveltexx
 PRODUCT_DEVICE := jactiveltexx
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-I9295
+PRODUCT_MANUFACTURER := Samsung
 
