db74c700e6e458a5e77e3de65bc727942b5b190c
Spegelius <spegelius@gmail.com>
2014-11-22 12:56:56 +0200

    Assorted updates
    - added core overlays
    - updated camera overlays
    - updated props list
    - updated makefiles
    
    Change-Id: I2fe595c3dd791a7aa225364afc04464ffdabb5f3

--- full_jactivelte.mk
+++ full_jactivelte.mk
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+ 
 #
 # This file is the build configuration for a full Android
 # build for maguro hardware. This cleanly combines a set of
@@ -23,7 +23,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-# Inherit from jactivelte device #LeJay: Not sure if needed
+# Inherit from jactiveltexx device
 $(call inherit-product, device/samsung/jactivelte/device.mk)
 
 # Set those variables here to overwrite the inherited values.
