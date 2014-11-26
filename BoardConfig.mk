1e92800102e22efbcbc8728cc9a150dff3501786
Spegelius <spegelius@gmail.com>
2014-11-26 19:41:04 +0200

    Initial multirom device tree
    Change-Id: I8813eb900f8b1716dceb1d5b38ee7de0ee965a13

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,3 +45,26 @@ TARGET_ADD_ISO_MODE_HJR := true
 TARGET_NEED_PREVIEW_SIZE_FIXUP := false
 TARGET_NEED_SAMSUNG_CAMERA_MODE := true
 TARGET_VIDEO_PREVIEW_ALWAYS_MAX := true
+
+# twrp
+TARGET_RECOVERY_FSTAB := device/samsung/jactivelte/twrp/twrp.fstab
+DEVICE_RESOLUTION := 1080x1920
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_INTERNAL_STORAGE_PATH := "/data/media/0"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_SAMSUNG := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p29"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,noauto_da_alloc,discard,journal_async_commit,errors=panic wait,check,encryptable=footer"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
+TW_BRIGHTNESS_PATH := /sys/devices/platform/msm_fb.526593/leds/lcd-backlight/brightness
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
